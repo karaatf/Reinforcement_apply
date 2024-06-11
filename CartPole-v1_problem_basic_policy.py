771acc9f657a20f51ca5b0439281261ccb163d37
karaatf <atfkara19@gmail.com>
2024-06-11 17:54:31 +0300

    CartPole problemi çözmek için basit bir politika eklendi

---
+++ CartPole-v1_problem_basic_policy.py
@@ -0,0 +1,36 @@
+import random
+import gym
+import numpy as np
+import pygame
+
+env = gym.make("CartPole-v1")
+obs = env.reset()
+
+
+def policy(obs):
+    angle = obs[2]
+    if angle<0:
+        return 0
+    else:
+        return 1
+
+
+totals=[]
+
+
+for episode in range(1000):
+    episode_rewards = 0
+    obs = env.reset()
+    for step in range(200):
+        action = policy(obs)
+        obs, reward, done, info = env.step(action)
+        episode_rewards +=reward
+        if done:
+            break
+    totals.append(episode_rewards)
+
+print(np.mean(totals),np.std(totals),np.min(totals),np.max(totals))
+
+env.close()
+
+
